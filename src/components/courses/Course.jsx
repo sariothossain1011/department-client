6b29e48e87b758c09b0c01220c8cffd6a5cc2c60
Sariot Hossain <sariothossain1011@gmail.com>
2023-11-06 22:10:58 +0600

    add tailwind css package

---
+++ src/components/courses/Course.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+
+const Course = () => {
+  return (
+    <Fragment>
+      <div>
+        <h1>All Couses</h1>
+        <p className='self-center whitespace-nowrap text-xl font-semibold text-red-500'>lorem hello </p>
+      </div>
+    </Fragment>
+  )
+}
+
+export default Course
--- src/pages/header/HeaderPage.jsx
+++ src/pages/header/HeaderPage.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Header from '../../components/header/Header'
+import Bannar from '../../components/bannar/Bannar'
+import About from '../../components/about/About'
+import Teacher from '../../components/teachers/Teacher'
+import Course from '../../components/courses/Course'
+import Achievement from '../../components/achievement/Achievement'
+import Gallery from '../../components/gallery/Gallery'
+import Blog from '../../components/blogs/Blog'
+import Footer from '../../components/footer/Footer'
 
 const HeaderPage = () => {
   return (
-    <div>HeaderPage</div>
+    <Fragment>
+      <Header/>
+      <Bannar/>
+      <About/>
+      <Teacher/>
+      <Course/>
+      <Achievement/>
+      <Gallery/>
+      <Blog/>
+      <Footer/>
+
+    </Fragment>
   )
 }
 
