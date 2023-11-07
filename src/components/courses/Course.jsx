42f9d5b0353e0681767bd2e63d61b9ac21d8fd18
Sariot Hossain <sariothossain1011@gmail.com>
2023-11-07 14:09:44 +0600

    design courses section

--- src/components/courses/Course.jsx
+++ src/components/courses/Course.jsx
@@ -1,11 +1,21 @@
 import React, { Fragment } from 'react'
+import CourseCard from './CourseCard'
 
 const Course = () => {
   return (
     <Fragment>
-      <div>
-        <h1>All Couses</h1>
-        <p className='self-center whitespace-nowrap text-xl font-semibold text-red-500'>lorem hello </p>
+      <div className=' container'>
+        <div className='text-red-500 text-center'>
+          <h1 className='h-5'>All Courses</h1>
+        </div>
+        <div className='mx-8 my-4 grid grid-cols-3 gap-4 justify-between '>
+          <CourseCard/>
+          <CourseCard/>
+          <CourseCard/>
+          <CourseCard/>
+          <CourseCard/>
+          <CourseCard/>
+        </div>
       </div>
     </Fragment>
   )
---
+++ src/components/courses/CourseCard.jsx
@@ -0,0 +1,32 @@
+import React, { Fragment } from "react";
+
+const CourseCard = () => {
+  return (
+    <Fragment>
+      <div className="card w-100  bg-base-100 shadow-xl ">
+        <figure>
+          <img src="../../public/course_image1.jpg" alt="Shoes" />
+        </figure>
+        <div className="card-body indicator ">
+        <span class="indicator-item indicator-start ms-20 badge badge-secondary p-3">First Semester</span>
+          <div className="card-title ">
+            <h1>First semester all couses here learn more</h1>
+          </div>
+          <div className="card-actions justify-between">
+            <div class="rating">
+              <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+              <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" checked />
+              <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+              <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+              <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+            </div>
+            <div className=""> 30 vodes</div>
+            <div className="">300+ students</div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default CourseCard;
