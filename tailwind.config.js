e28a2d0083549ea83b927e446cc8f43a4666990a
MdFahad80 <mdfahad8.me@gmail.com>
2023-11-07 16:39:08 +0600

    Header Banner Components

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    letterSpacing: {
+      tightest: '-.18px',
+      tighter: '-.15px',
+      tight: '-.025em',
+      normal: '0',
+      wide: '.025em',
+      wider: '.05em',
+      widest: '.1em',
+      widest: '.25em',
+    },
     extend: {},
   },
   plugins: [],
