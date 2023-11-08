7385a120ad4b9b05334fe36a6261a6f188c48b53
rana-arju <arjurana20@gmail.com>
2023-11-08 21:38:27 +0600

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import about from "../../assets/about.jpg"
 const About = () => {
   return (
-    <div>About</div>
-  )
+    <div>
+      <div className="card lg:card-side bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src={about}
+            alt="Album"
+          className='h-96'
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">New album is released!</h2>
+          <p>Click the button to listen on Spotiwhy app.</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Listen</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About
