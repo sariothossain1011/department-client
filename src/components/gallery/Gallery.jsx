7b22f329ff7736af60bcfbfa45cef1e001fbae19
MdFahad80 <mdfahad8.me@gmail.com>
2023-11-11 18:38:39 +0600

    Change Gallery Design

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,44 +1,23 @@
-import React from "react";
-import "lightbox.js-react/dist/index.css";
-import { SlideshowLightbox } from "lightbox.js-react";
+import React from 'react'
+import 'lightbox.js-react/dist/index.css'
+import {SlideshowLightbox} from 'lightbox.js-react'
 
 const Gallery = () => {
   return (
     <>
-      <div className="text-center font-sans font-bold text-[36px] py-4">
-        Gallery
-      </div>
-      <SlideshowLightbox
-        className="container grid grid-cols-3 gap-2 mx-auto"
-        showThumbnails={true}
-      >
-        <img
-          className="w-full rounded"
-          src="https://images.pexels.com/photos/580151/pexels-photo-580151.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        />
-        <img
-          className="w-full rounded"
-          src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-        />
-        <img
-          className="w-full rounded"
-          src="https://images.pexels.com/photos/13208323/pexels-photo-13208323.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        />
-        <img
-          className="w-full rounded"
-          src="https://images.pexels.com/photos/580151/pexels-photo-580151.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        />
-        <img
-          className="w-full rounded"
-          src="https://images.pexels.com/photos/13996896/pexels-photo-13996896.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        />
-        <img
-          className="w-full rounded"
-          src="https://images.pexels.com/photos/13208323/pexels-photo-13208323.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        />
+      <div className="text-center font-sans font-bold text-[36px] py-4">Gallery</div>
+      <SlideshowLightbox className='container grid grid-cols-2 md:grid-cols-3 min-[1600px]:grid-cols-4 gap-4 mx-auto max-[1024px]:px-[20px]' showThumbnails={true}>
+        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt=""/>
+        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt=""/>
+        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt=""/>
+        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt=""/>
+        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt=""/>
+        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt=""/>
+        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg" alt=""/>
+        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg" alt=""/>
       </SlideshowLightbox>
     </>
-  );
-};
+  )
+}
 
-export default Gallery;
+export default Gallery
