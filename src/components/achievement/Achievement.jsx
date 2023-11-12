30bc9ef574d8d2633006e46d37a62b5ad4fb075b
MdFahad80 <mdfahad8.me@gmail.com>
2023-11-12 09:31:50 +0600

    Desing notice Board

--- src/components/achievement/Achievement.jsx
+++ src/components/achievement/Achievement.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import {BsFillPlayFill} from "react-icons/bs"
 
 const Achievement = () => {
   return (
-    <div>Achievement</div>
+    <div className='container mx-auto py-10 max-[1024px]:px-[20px]'>
+      <h1 className='text-[25px] font-sans font-bold'>Notice Board</h1>
+      <div className='ml-3 pt-4'>
+        <a class="d-flex align-items-center gap-2" role="button" href="./ReactJSNotesForProfessionals.pdf" download="./ReactJSNotesForProfessionals.pdf"> <BsFillPlayFill className=' text-teal-900 text-[22px]' /> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, eligendi.. </a>
+        <a class="d-flex align-items-center gap-2" role="button" href="./ReactJSNotesForProfessionals.pdf" download="./ReactJSNotesForProfessionals.pdf"> <BsFillPlayFill className=' text-teal-900 text-[22px]' /> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, eligendi.. </a>
+        <a class="d-flex align-items-center gap-2" role="button" href="./ReactJSNotesForProfessionals.pdf" download="./ReactJSNotesForProfessionals.pdf"> <BsFillPlayFill className=' text-teal-900 text-[22px]' /> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, eligendi.. </a>
+        <a class="d-flex align-items-center gap-2" role="button" href="./ReactJSNotesForProfessionals.pdf" download="./ReactJSNotesForProfessionals.pdf"> <BsFillPlayFill className=' text-teal-900 text-[22px]' /> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, eligendi.. </a>
+      </div>
+    </div>
   )
 }
 
