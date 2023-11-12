55dea28e6b27c261bedfdcfac8ff4ef7e8f1bb23
Hridoy-shill <ridoyaaa60@gmail.com>
2023-11-12 20:40:27 +0600

    again for testing

--- src/components/achievement/Achievement.jsx
+++ src/components/achievement/Achievement.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Achievement = () => {
   return (
     <>
-      <div className='text-5xl w-fit text-center border-b-4 border-[#F7A510] mx-auto pb-2'>Achievement is coming soon.....</div>
+      <div className='text-5xl w-fit text-center border-b-4 border-[#F7A510] mx-auto pb-2'>Achievement is coming</div>
       <p>here is the achievement section--</p>  
     </>
   )
