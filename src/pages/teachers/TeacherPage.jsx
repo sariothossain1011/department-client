f5e2c76af59c6c578821ffeadf7ccc8bbe8d45b4
shefaitur <121374560+shefai-tur@users.noreply.github.com>
2023-11-12 21:29:03 +0600

    Our Teacher

--- src/pages/teachers/TeacherPage.jsx
+++ src/pages/teachers/TeacherPage.jsx
@@ -1,4 +1,16 @@
 import React from 'react'
+import Teacher from '../../components/teachers/Teacher'
+
+const TeacherPages = () => {
+  return (
+   <>
+    <Teacher/>
+   </>
+  )
+}
+
+export default TeacherPages
+import React from 'react'
 import OurTeacher from '../../components/teacher/OurTeacher'
 import {IoIosArrowForward} from "react-icons/io"
 import { Link } from "react-router-dom";
@@ -7,14 +19,14 @@ const TeacherPages = () => {
     <>
     <div className='bg-blog-banner bg-center '>
     <div className="container-mx-auto bg-black bg-opacity-30">
-    <h1 className=" text-white flex justify-center text-4xl font-bold mb-5 pt-36 ">BLOG</h1>
+    <h1 className=" text-white flex justify-center text-4xl font-bold mb-5 pt-36 ">Our Teacher</h1>
      <div className="flex gap-4 justify-center pb-40">
      <Link to="/" >
      <h1 className=" text-white">HOME</h1>
      </Link>
       
       <IoIosArrowForward className="text-white mt-1"/>
-      <h1 className=" text-white">BLOG</h1>
+      <h1 className=" text-white">Our Teacher</h1>
      </div>
     </div>
     </div>
