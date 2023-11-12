2f62d2405a72492d54ed86e39b3eabb7fea4ba62
shefaitur <121374560+shefai-tur@users.noreply.github.com>
2023-11-12 21:18:53 +0600

    Our Teacher page

---
+++ src/components/teacher/OurTeacher.jsx
@@ -0,0 +1,137 @@
+import React from "react";
+import {BiLogoFacebook,BiLogoInstagram} from "react-icons/bi"
+import {RiTwitterXFill} from "react-icons/ri"
+const OurTeacher = () => {
+  return (
+    <div className="container mx-auto">
+      <h2 className="text-4xl text-center font-bold capitalize my-10">
+        Our Teacher
+      </h2>
+<div className="flex justify-center flex-wrap gap-20 py-10">
+<div className="card w-80 bg-base-100 shadow-xl">
+  <figure><img className=" rounded-full w-40 h-40" src="https://htmldemo.zcubethemes.com/qeducato/img/team/team09.png" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title text-titel justify-center hover:text-secondary duration-500">Howard Holmes</h2>
+    <h3 className=" text-center text-secondary ">CEO & Founder</h3>
+   
+ <div className=" flex justify-center gap-5 py-5">
+
+<BiLogoFacebook className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+<BiLogoInstagram className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+<RiTwitterXFill className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+
+ </div>
+  </div>
+</div>
+<div className="card w-80 bg-base-100 shadow-xl">
+  <figure><img className=" rounded-full w-40 h-40" src="https://htmldemo.zcubethemes.com/qeducato/img/team/team03.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title text-titel justify-center hover:text-secondary duration-500">Howard Holmes</h2>
+    <h3 className=" text-center text-secondary ">CEO & Founder</h3>
+   
+ <div className=" flex justify-center gap-5 py-5">
+
+<BiLogoFacebook className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+<BiLogoInstagram className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+<RiTwitterXFill className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+
+ </div>
+  </div>
+</div>
+<div className="card w-80 bg-base-100 shadow-xl">
+  <figure><img className=" rounded-full w-40 h-40" src="https://htmldemo.zcubethemes.com/qeducato/img/team/team10.png" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title text-titel justify-center hover:text-secondary duration-500">Howard Holmes</h2>
+    <h3 className=" text-center text-secondary ">CEO & Founder</h3>
+   
+ <div className=" flex justify-center gap-5 py-5">
+
+<BiLogoFacebook className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+<BiLogoInstagram className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+<RiTwitterXFill className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+
+ </div>
+  </div>
+</div>
+<div className="card w-80 bg-base-100 shadow-xl">
+  <figure><img className=" rounded-full w-40 h-40" src="https://htmldemo.zcubethemes.com/qeducato/img/team/team11.png" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title text-titel justify-center hover:text-secondary duration-500">Howard Holmes</h2>
+    <h3 className=" text-center text-secondary ">CEO & Founder</h3>
+   
+ <div className=" flex justify-center gap-5 py-5">
+
+<BiLogoFacebook className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+<BiLogoInstagram className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+<RiTwitterXFill className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+
+ </div>
+  </div>
+</div>
+<div className="card w-80 bg-base-100 shadow-xl">
+  <figure><img className=" rounded-full w-40 h-40" src="https://htmldemo.zcubethemes.com/qeducato/img/team/team09.png" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title text-titel justify-center hover:text-secondary duration-500">Howard Holmes</h2>
+    <h3 className=" text-center text-secondary ">CEO & Founder</h3>
+   
+ <div className=" flex justify-center gap-5 py-5">
+
+<BiLogoFacebook className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+<BiLogoInstagram className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+<RiTwitterXFill className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+
+ </div>
+  </div>
+</div>
+<div className="card w-80 bg-base-100 shadow-xl">
+  <figure><img className=" rounded-full w-40 h-40" src="https://htmldemo.zcubethemes.com/qeducato/img/team/team12.png" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title text-titel justify-center hover:text-secondary duration-500">Howard Holmes</h2>
+    <h3 className=" text-center text-secondary ">CEO & Founder</h3>
+   
+ <div className=" flex justify-center gap-5 py-5">
+
+<BiLogoFacebook className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+<BiLogoInstagram className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+<RiTwitterXFill className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+
+ </div>
+  </div>
+</div>
+<div className="card w-80 bg-base-100 shadow-xl">
+  <figure><img className=" rounded-full w-40 h-40" src="https://htmldemo.zcubethemes.com/qeducato/img/team/team09.png" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title text-titel justify-center hover:text-secondary duration-500">Howard Holmes</h2>
+    <h3 className=" text-center text-secondary ">CEO & Founder</h3>
+   
+ <div className=" flex justify-center gap-5 py-5  ">
+
+<BiLogoFacebook className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+<BiLogoInstagram className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+<RiTwitterXFill className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+
+ </div>
+  </div>
+</div>
+<div className="card w-80 bg-base-100 shadow-xl">
+  <figure><img className=" rounded-full w-40 h-40" src="https://htmldemo.zcubethemes.com/qeducato/img/team/team09.png" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title text-titel justify-center hover:text-secondary duration-500">Howard Holmes</h2>
+    <h3 className=" text-center text-secondary ">CEO & Founder</h3>
+   
+ <div className=" flex justify-center gap-5 py-5">
+
+<BiLogoFacebook className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+<BiLogoInstagram className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+<RiTwitterXFill className=" w-10 h-10 rounded-full border hover:text-white hover:bg-secondary duration-500 "/>
+
+ </div>
+  </div>
+</div>
+
+</div>
+    </div>
+  );
+};
+
+export default OurTeacher;
