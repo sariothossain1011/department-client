09dd3ce2d96d7dfc75421374322416561690267c
Sariot Hossain <sariothossain1011@gmail.com>
2023-12-04 11:22:13 +0600

    pull reqest by sariot 04-12-23

--- src/helpers/secret.js
+++ src/helpers/secret.js
@@ -1,5 +1,5 @@
 
 
-export const API = "http://localhost:8080/api/v1/"
+// export const API = "http://localhost:8080/api/v1/"
 
-//export const API = "https://department-server-tau.vercel.app/api/v1/"
+export const API = "https://department-server-tau.vercel.app/api/v1/"
